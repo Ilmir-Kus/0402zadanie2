9e814d69ca221156bbe9ed741edf423653f1a91f
Neto test <ilmir.kusmaev03@mail.ru>
2023-02-09 13:53:58 +0400

    Commit 2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
     public double bms(int mass, double height) {
-     double index = mass / (height * height);
-     return index;
+        double index = mass / (height * height);
+        return index;
     }
 }
